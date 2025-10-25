1792cb307f6562585c0063bf0189e069e494cef4
Mohamad Lingga Syahputra <mohamadlingga275@gmail.com>
2025-10-25 19:40:47 +0700

    wifi done

--- wifi_connection/src/main.cpp
+++ wifi_connection/src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-String ssid = "hah"; //ssid wifi yang di konek
-String pw = "hahahaha"; //password dari ssid wifi yang digunakan
+// String ssid = "haha"; //ssid wifi yang di konek
+// String pw = "hahahaha"; //password dari ssid wifi yang digunakan
 void setup() {
   // put your setup code here, to run once:
-  WiFi.begin(ssid.c_str(), pw.c_str()); //connect ke wifi
+  // WiFi.begin(ssid.c_str(), pw.c_str()); //connect ke wifi
   Serial.begin(9600); //memulai komunikasi UART dengan kecepatan transfer 9600bit
-  Serial.println("connecting..");
+  // Serial.println("connecting..");
+  // while (WiFi.status() != WL_CONNECTED){//cek apakah status wifi tidak konek?
+  //   Serial.print(".");//tampilkan .
+  //   delay(500);
+  // }
+  WiFi.mode(WIFI_AP_STA);// uba mode wifi 
+  WiFi.beginSmartConfig(); // jalankan wifi
+  while (!WiFi.smartConfigDone()){//cek apakah status wifi sudah koneka?
+    Serial.print("-");//tampilkan .
+    delay(500);
+  }
   while (WiFi.status() != WL_CONNECTED){//cek apakah status wifi tidak konek?
     Serial.print(".");//tampilkan .
     delay(500);
   }
   Serial.println("wifi connected");
+  Serial.println(WiFi.SSID());
+  Serial.println(WiFi.RSSI());
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.dnsIP());
 }
 
 void loop() {
