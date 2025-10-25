aba4f88b60440929688a4707c4be4aaff01c6d84
Mohamad Lingga Syahputra <mohamadlingga275@gmail.com>
2025-10-25 19:12:18 +0700

    add 1

--- wifi_connection/src/main.cpp
+++ wifi_connection/src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+String ssid = "hah"; //ssid wifi yang di konek
+String pw = "hahahaha"; //password dari ssid wifi yang digunakan
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  WiFi.begin(ssid.c_str(), pw.c_str()); //connect ke wifi
+  Serial.begin(9600); //memulai komunikasi UART dengan kecepatan transfer 9600bit
+  Serial.println("connecting..");
+  while (WiFi.status() != WL_CONNECTED){//cek apakah status wifi tidak konek?
+    Serial.print(".");//tampilkan .
+    delay(500);
+  }
+  Serial.println("wifi connected");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
